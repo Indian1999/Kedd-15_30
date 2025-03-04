6d757ade8468419989ad45afe16669a348d0a825
Indian1999 <haszab99@gmail.com>
2025-03-04 16:28:13 +0100

    myPackage létrehozva

---
+++ saját_modul/main.py
@@ -0,0 +1,4 @@
+from myPackage import functions
+
+print(functions.add(4,5))
+print(functions.power(2,3))
---
+++ saját_modul/myPackage/functions.py
@@ -0,0 +1,8 @@
+def add(x,y):
+    return x+y
+
+def average(x,y):
+    return add(x,y) / 2
+
+def power(x,y):
+    return x ** y
