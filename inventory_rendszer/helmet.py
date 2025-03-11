37be55cab9cc1362d838dbbb31819e27909cd345
Indian1999 <haszab99@gmail.com>
2025-03-11 16:50:30 +0100

    helmet

--- inventory_rendszer/helmet.py
+++ inventory_rendszer/helmet.py
@@ -3,5 +3,8 @@ from rarity import Rarity
 from stats import Stats
 
 class Helmet(Equippable):
-    def __init__(self, name:str, rarity:Rarity = Rarity.COMMON, vendorSellPrice:int = 100, minimumLevel:int = 1, stats:Stats = Stats())-> None:
+    def __init__(self, name:str, rarity:Rarity = Rarity.COMMON, vendorSellPrice:int = 100, minimumLevel:int = 1, stats:Stats = Stats(), maxDurability:int = 100)-> None:
         super().__init__(name, rarity, vendorSellPrice, minimumLevel)
+        self.stats = stats
+        self.maxDurability = maxDurability
+        self.durability = maxDurability
