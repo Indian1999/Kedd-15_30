51cccb2f6623b5cd3210a463128f28c0bf0a6db7
Indian1999 <haszab99@gmail.com>
2025-03-11 17:06:18 +0100

    inventory

---
+++ inventory_rendszer/boots.py
@@ -0,0 +1,10 @@
+from equippable import Equippable
+from rarity import Rarity
+from stats import Stats
+
+class Boots(Equippable):
+    def __init__(self, name:str, rarity:Rarity = Rarity.COMMON, vendorSellPrice:int = 100, minimumLevel:int = 1, stats:Stats = Stats(), maxDurability:int = 100)-> None:
+        super().__init__(name, rarity, vendorSellPrice, minimumLevel)
+        self.stats = stats
+        self.maxDurability = maxDurability
+        self.durability = maxDurability
---
+++ inventory_rendszer/inventory.py
@@ -0,0 +1,22 @@
+from item import Item
+
+class Inventory:
+    def __init__(self, size:int = 16, items: list[Item] = [], money:int = 0):
+        self.size = size
+        self.items = items
+        self.money = money
+        
+    def addItem(self, item: Item):
+        if len(self.items) < self.size:
+            self.items.append(item)
+        else:
+            print("Inventory is full!")
+    
+    def removeItem(self, item: Item):
+        for i in self.items:
+            if i == item:
+                self.items.remove(item)
+                return
+        print("This item is not in the inventory!")
+        
+        
