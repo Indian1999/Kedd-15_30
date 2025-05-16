5011fefe01aa35512720a2d523aade4c6d22ed56
Indian1999 <haszab99@gmail.com>
2025-05-16 19:42:58 +0200

    event kezelés

--- snake.py
+++ snake.py
@@ -23,15 +23,48 @@ SPEED = 15
 game_over_font = pygame.font.SysFont("Arial", 25)
 score_font = pygame.font.SysFont("Arial", 35)
 
+def random_pos():
+    x = random.randrange(0, WINDOW_WIDTH, PIXEL_SIZE)
+    y = random.randrange(0, WINDOW_HEIGHT, PIXEL_SIZE)
+    return x, y
+
 def gameloop():
     game_over = False
     application_close = False
+    
+    x_snake = WINDOW_WIDTH // 2
+    y_snake = WINDOW_HEIGHT // 2
+    
+    x_change = 0
+    y_change = 0
+    
+    snake = []
+    length_of_snake = 1
+    
+    x_food, y_food = random_pos()
     
     while not application_close:
         clock.tick(SPEED)
         while game_over:
             # Kiírjuk hogy új játék vagy bezárás
             pass
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                application_close = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -PIXEL_SIZE
+                    y_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x_change = PIXEL_SIZE
+                    y_change = 0
+                elif event.key == pygame.K_UP:
+                    x_change = 0
+                    y_change = -PIXEL_SIZE
+                elif event.key == pygame.K_DOWN:
+                    x_change = 0
+                    y_change = PIXEL_SIZE
         
     pygame.quit()
     quit()
